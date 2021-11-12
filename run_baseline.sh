9afec45ecf54b1758ef559e22d4e1bcc8039d455
Philipp Benz <phibenz@gmail.com>
2021-11-13 02:14:06 +0900

    philipp run simco

--- run_baseline.sh
+++ run_baseline.sh
@@ -1,9 +1,9 @@
 python main_moco.py \
   -a resnet18 \
-  --lr 0.015 \
+  --lr 0.03 \
   --epochs 100 \
-  --method simmoco \
-  --batch-size 128 \
+  --method simco \
+  --batch-size 256 \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
   --mlp --moco-t 0.2 --aug-plus --cos \
   /workspace/ssd2_4tb/data/imagenet_pytorch
